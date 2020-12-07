d66cdfe39e378b22262ebb87b2f0e0f3aef63f94
zoz0312 <zoz0312@naver.com>
2020-12-07 18:28:04 +0900

    route page add

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,51 +1,16 @@
 import React from 'react';
-import { isLoggedInVar } from '../apollo';
-import { useForm } from 'react-hook-form';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
+import CreateAccount from '../pages/CreateAccount';
 
 const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, errors } = useForm();
-
-  const onSubmit = () => {
-    console.log(watch());
-  }
-
-  const onInvalid = (data: any) => {
-    console.log('data', data)
-    console.log(`can't create`);
-  }
-
-  console.log('errors', errors)
-
   return (
-    <div>
-      <span>LoggedOutRouter</span>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            ref={register({
-              required: true,
-              validate: (email: string) => email.includes('@gmail.com'),
-            })}
-            type="email"
-            name="email"
-            required
-            placeholder="email"
-          />
-        </div>
-        <div>
-          <input
-            ref={register({
-              required: true,
-            })}
-            type="password"
-            name="password"
-            required
-            placeholder="password"
-          />
-        </div>
-        <button>Click to login</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/create-account' component={CreateAccount}/>
+        <Route path='/' component={Login}/>
+      </Switch>
+    </Router>
   )
 }
 
