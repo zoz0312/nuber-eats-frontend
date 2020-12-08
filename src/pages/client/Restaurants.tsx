08bedf7bc8abbbe6b283ebf6e34a00f01abc6c9b
zoz0312 <zoz0312@naver.com>
2020-12-08 18:01:51 +0900

    restaurants list

--- src/pages/client/Restaurants.tsx
+++ src/pages/client/Restaurants.tsx
@@ -34,7 +34,8 @@ const RESTAURANTS_QUERY = gql`
   }
 `;
 
-const defaultImageURL = 'https://www.nicepng.com/png/full/131-1314271_food-icon-food-court-icon-png.png';
+const defaultCategoryImage = 'https://www.nicepng.com/png/full/131-1314271_food-icon-food-court-icon-png.png';
+const defaultRestaurantImage = 'https://www.bbq.co.kr/images/common/logo_header_bbq.png';
 
 const Restaurants: React.FC = () => {
   const { data, loading, error } = useQuery<
@@ -63,15 +64,26 @@ const Restaurants: React.FC = () => {
           <div className="max-x-screen-2xl mx-auto mt-8">
             <div className="flex justify-around max-w-xs mx-auto">
             {data?.allcategories.categories?.map((category, index) => (
-              <div className="w-14 flex flex-col items-center">
+              <div className="w-16 flex flex-col items-center group">
                 <div
                   key={index}
-                  style={{backgroundImage: `url(${category.coverImage ? category.coverImage : defaultImageURL})`}}
-                  className="w-14 h-14 rounded-full bg-cover hover:bg-gray-100 cursor-pointer">
+                  style={{backgroundImage: `url(${category.coverImage ? category.coverImage : defaultCategoryImage})`}}
+                  className="w-16 h-16 rounded-full bg-cover group-hover:bg-gray-100 cursor-pointer">
                 </div>
                 <span className="mt-1 text-sm text-center font-bold break-all">{ category.name }</span>
               </div>
             ))}
+            </div>
+            <div className="grid grid-cols-3 gap-x-5 gap-y-10 mt-10">
+              {data?.restaurants.results?.map((restaurant, index) => (
+                <div key={index}>
+                  <div
+                    style={{backgroundImage:`url(${restaurant.coverImage ? restaurant.coverImage : defaultRestaurantImage})`}}
+                    className="bg-red-500 py-28 bg-cover bg-center mb-2"></div>
+                  <h3 className="text-xl font-semibold">{ restaurant.name }</h3>
+                  <span className="border-t-2 border-lime-500">{ restaurant.category?.name }</span>
+                </div>
+              ))}
             </div>
           </div>
         )}
