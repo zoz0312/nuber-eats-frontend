fb452e1d8cf1671c1a8e465338d50c413b5c2623
DESKTOP-NUTKK5H\zoz03 <zoz0312@naver.com>
2020-12-11 20:15:10 +0900

    login testing add

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,9 +1,33 @@
-const HOST = 'http://localhost:3000'
 
-describe('First Test', () => {
-  it('should go to homepage', () => {
-    cy.visit(HOST)
+describe('Login', () => {
+  it('should see login page', () => {
+    cy.visit('/')
       .title()
       .should('eq', 'Login | Nuber Eats');
   });
+
+  it('can see email & password validation error', () => {
+    cy.visit('/');
+    cy.findByPlaceholderText(/email/i).type('test@gmail');
+    cy.findByRole('alert').should('have.text', 'Please enter a valid email');
+    cy.findByPlaceholderText(/email/i).clear();
+    cy.findByRole('alert').should('have.text', 'Email is required');
+    cy.findByPlaceholderText(/email/i).type('test@gmail.com');
+    cy.findByPlaceholderText(/password/i)
+      .type('123123')
+      .clear();
+    cy.findByRole('alert').should('have.text', 'Password is required');
+  });
+
+  it('can fill out the form', () => {
+    cy.visit('/');
+    cy.findByPlaceholderText(/email/i).type('aju.an@gmail.com');
+    cy.findByPlaceholderText(/password/i).type('121212');
+    cy.findByRole('button').should('not.have.class', 'pointer-events-none').click();
+    cy.window().its('localStorage.nuber-token').should('be.a', 'string');
+  });
+
+  it('sign up', () => {
+    cy.visit('/create-account')
+  })
 });
