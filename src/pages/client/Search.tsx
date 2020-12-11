82c3091d5336e819d928c764e62e28b5fb4f32ee
zoz0312 <zoz0312@naver.com>
2020-12-11 16:45:51 +0900

    search page add

--- src/pages/client/Search.tsx
+++ src/pages/client/Search.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { gql, useLazyQuery } from '@apollo/client';
+import { gql, useLazyQuery, useQuery } from '@apollo/client';
 import { RESTUARANT_FRAGMENT } from '../../fragments';
-import { searchRestaurant, searchRestaurantVariables } from '../../__generated__/searchRestaurant';
+import { searchRestaurant, searchRestaurantVariables, searchRestaurant_searchRestaurant_restaurants } from '../../__generated__/searchRestaurant';
+import Restaurant from '../../components/Restaurant';
+import useScrollPage from '../../hooks/useScrollPage';
 
 const SEARCH_RESTAURANT = gql`
   query searchRestaurant($input: SearchRestaurantInput!) {
@@ -23,31 +25,82 @@ const SEARCH_RESTAURANT = gql`
 const ClientSearch: React.FC = () => {
   const location = useLocation();
   const history = useHistory();
-  const [startQuery, { loading, data }] = useLazyQuery<
+  const query = location.search.split('?term=')[1];
+  const [page, setPage] = useState(1);
+  const [itemList, setItemList] = useState<searchRestaurant_searchRestaurant_restaurants[]>([]);
+  const scrollState = useScrollPage();
+  const { loading, data } = useQuery<
     searchRestaurant,
     searchRestaurantVariables
-  >(SEARCH_RESTAURANT);
+  >(SEARCH_RESTAURANT,{
+    variables: {
+      input: {
+        page,
+        query,
+      }
+    }
+  });
+
+  // startQuery({
+  //   variables: {
+  //     input: {
+  //       page,
+  //       query,
+  //     }
+  //   }
+  // });
 
   useEffect(() => {
-    const query = location.search.split('?term=')[1];
     if (!query) {
       return history.replace('/');
     }
-    startQuery({
-      variables: {
-        input: {
-          page: 1,
-          query,
-        }
-      }
-    });
   }, [history, location]);
+
+  useEffect(() => {
+    if (data && data.searchRestaurant.restaurants) {
+      const { searchRestaurant: { restaurants } } = data;
+      if (restaurants) {
+        setItemList([
+          ...itemList,
+          ...restaurants,
+        ]);
+      }
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (!data || !data.searchRestaurant.totalPages) {
+      return;
+    }
+    const { y, screenY } = scrollState;
+    if (y === screenY) {
+      if (!loading && page < data.searchRestaurant.totalPages) {
+        setPage(current => current + 1)
+      }
+    }
+  }, [scrollState]);
 
   return (
     <div>
       <Helmet>
         <title>Search | Nuber Eats</title>
       </Helmet>
+      <header className="bg-gray-200 w-screen py-10 text-center">
+        <h4 className="text-3xl">{ query } / {`${data?.searchRestaurant.totalResults} Restaurants`}</h4>
+      </header>
+      <article>
+        <div className="grid md:grid-cols-3 gap-x-5 gap-y-10 pb-10 mt-10 mb-10">
+          {itemList.map(restaurant => (
+            <Restaurant
+              key={restaurant.id}
+              id={restaurant.id}
+              coverImage={restaurant.coverImage}
+              name={restaurant.name}
+              categoryName={restaurant.category?.name || ''}
+            />
+          ))}
+        </div>
+      </article>
     </div>
   );
 }
